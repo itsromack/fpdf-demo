862b19832049951fcb612d08d83a599e9d615850
Romack Natividad <romack@vesanique.com.au>
2022-11-02 12:23:40 +0800

    Downloadable pdf file

--- tutorial7.php
+++ tutorial7.php
@@ -8,7 +8,12 @@ $pdf->AddFont('Ringbearer');
 $pdf->AddPage();
 $pdf->SetFont('Ringbearer','',35);
 $pdf->Write(10,'The Lord Of The Rings');
-$pdf->Output();
+
+$pdf->Ln(20);
+
+$pdf->SetFont('Ringbearer','',15);
+$pdf->Write(10,'Written By John Ronald Reuel Tolkien');
+$pdf->Output('D', 'lotr.pdf');
 
 // Run php vendor/fpdf/fpdf/src/Fpdf/makefont/makefont.php fonts/ringbearer.ttf
 // Move the generated files to the font folder (vendor/fpdf/fpdf/src/Fpdf/font/)
